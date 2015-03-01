80a9a28c040f61caea5dd46434b7be43e5918636
Evan Turner <evturn@gmail.com>
2015-03-01 13:33:30 -0500

    TodoList given functions to check if all models are completed

--- js/TodoMVC.Todos.js
+++ js/TodoMVC.Todos.js
@@ -25,7 +25,18 @@ TodoMVC.module('Todos', function(Todos, App, Backbone, Marionette, $, _) {
   Todos.TodoList = Backbone.Collection.extend({
     model: Todos.Todo,
     localStorage: new Backbone.LocalStorage(localStorageKey),
-
+    getCompleted: function() {
+      return this.filter(this._isCompleted);
+    },
+    getActive: function() {
+      return this.reject(this._isCompleted);
+    },
+    comparator: function(todo) {
+      return todo.get('created');
+    },
+    _isCompleted: function(todo) {
+      return todo.isCompleted();
+    }
   });
 
 });
