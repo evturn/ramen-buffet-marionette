9045a293c30a7731427c9642bea1d7d189afa90c
Evan Turner <evturn@gmail.com>
2015-03-01 13:26:19 -0500

    added an update function for list view and function to toggle checked todos to 'complete

--- js/TodoMVC.TodoList.Views.js
+++ js/TodoMVC.TodoList.Views.js
@@ -78,6 +78,20 @@ TodoMVC.module('TodoList.Views', function(Views, App, Backbone, Marionette, $, _
     onRender: function() {
       this.update();
     },
+    update: function() {
+      function reduceCompleted(left, right) { 
+        return left && right.get('completed'); 
+      }      
+      var allCompleted = this.collection.reduce(reduceCompleted,true);
+      this.ui.toggle.prop('checked', allCompleted);
+      this.$el.parent().toggle(!!this.collection.length);
+    },
+    onToggleAllClick: function(e) {
+      var isChecked = e.currentTarget.checked;
+      this.collection.each(function(todo) {
+        todo.save({'completed': isChecked});
+      });
+    }
   });
 
   App.vent.on('todoList:filter',function(filter) {
