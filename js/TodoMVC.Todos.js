c82fd152592814d618d74fbfee40ba08f6e6e602
Evan Turner <evturn@gmail.com>
2015-03-01 13:29:53 -0500

    localStorage created in Model passing the localStorageKey in

--- js/TodoMVC.Todos.js
+++ js/TodoMVC.Todos.js
@@ -2,5 +2,10 @@ TodoMVC.module('Todos', function(Todos, App, Backbone, Marionette, $, _) {
 
   var localStorageKey = 'ramen-buffet-marionette';
 
+  Todos.Todo = Backbone.Model.extend({
+    localStorage: new Backbone.LocalStorage(localStorageKey),
+    
+  });
+
 
 });
