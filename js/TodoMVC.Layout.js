58235018bfe734f54c12df161dd10afea9beecdd
Evan Turner <evturn@gmail.com>
2015-03-01 13:05:42 -0500

    Layout.Footer added

--- js/TodoMVC.Layout.js
+++ js/TodoMVC.Layout.js
@@ -34,4 +34,44 @@ TodoMVC.module('Layout', function(Layout, App, Backbone, $, _) {
 		}
 	});
 
+
+	Layout.Footer = Marionette.Layout.extend({
+		template: '#template-footer',
+		ui: {
+			todoCount 		 : '#todo-count .count',
+			todoCountLabel : '#todo-count .label'
+			clearCount 		 : '#clear-completed .count',
+      filters 			 : "#filters a"
+    },
+    events: {
+      "click #clear-completed" : "onClearClick"
+    },
+    initialize: function() {
+      this.bindTo( App.vent, "todoList: filter", this.updateFilterSelection, this );
+      this.bindTo( this.collection, 'all', this.updateCount, this );
+    },
+    onRender: function() {
+      this.updateCount();
+    },
+    updateCount: function() {
+      var activeCount = this.collection.getActive().length,
+      completedCount = this.collection.getCompleted().length;
+      this.ui.todoCount.html(activeCount);
+      this.ui.todoCountLabel.html(activeCount === 1 ? 'item' : 'items');
+      this.ui.clearCount.html(completedCount === 0 ? '' : '(' + completedCount + ')');
+    },
+    updateFilterSelection: function( filter ) {
+      this.ui.filters
+        .removeClass('selected')
+        .filter( '[href="#' + filter + '"]')
+        .addClass( 'selected' );
+    },
+    onClearClick: function() {
+      var completed = this.collection.getCompleted();
+      completed.forEach(function destroy(todo) {
+        todo.destroy();
+      });
+    },
+  });
+
 });
