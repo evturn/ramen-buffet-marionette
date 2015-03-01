ca17e332810d65b35754dc66ad32f8b0babbfc39
Evan Turner <evturn@gmail.com>
2015-03-01 13:00:03 -0500

    Layout.Header extending Marionette's ItemView

--- js/TodoMVC.Layout.js
+++ js/TodoMVC.Layout.js
@@ -1,33 +1,37 @@
 TodoMVC.module('Layout', function(Layout, App, Backbone, $, _) {
-	template: '#template-header',
-	ui: {
-		input: '#new-todo'
-	},
-	events: {
-		'keypress #new-todo' : 'onInputKeypress',
-		'blur #new-todo'		 : 'onTodoBlur'
-	},
-	onTodoBlur: function() {
-		var todoText = this.ui.input.val().trim();
-		this.createTodo(todoText);
-	},
-  onInputKeypress: function(e) {
-    var ENTER_KEY = 13;
-    var todoText = this.ui.input.val().trim();
-    if ( e.which === ENTER_KEY && todoText ) {
-      this.createTodo(todoText);
-    }
-  },
-  completeAdd: function() {
-    this.ui.input.val('');
-  },
-  createTodo: function(todoText) {
-	  if (todoText.trim() === ""){ return; }
+	
+	Layout.Header = Backbone.Marionette.ItemView.extend({
+		template: '#template-header',
+		ui: {
+			input: '#new-todo'
+		},
+		events: {
+			'keypress #new-todo' : 'onInputKeypress',
+			'blur #new-todo'		 : 'onTodoBlur'
+		},
+		onTodoBlur: function() {
+			var todoText = this.ui.input.val().trim();
+			this.createTodo(todoText);
+		},
+	  onInputKeypress: function(e) {
+	    var ENTER_KEY = 13;
+	    var todoText = this.ui.input.val().trim();
+	    if ( e.which === ENTER_KEY && todoText ) {
+	      this.createTodo(todoText);
+	    }
+	  },
+	  completeAdd: function() {
+	    this.ui.input.val('');
+	  },
+	  createTodo: function(todoText) {
+		  if (todoText.trim() === ""){ return; }
 
-	  this.collection.create({
-	    title: todoText
-	  });
+		  this.collection.create({
+		    title: todoText
+		  });
 
-	  this.completeAdd();
-	}
+		  this.completeAdd();
+		}
+	});
+
 });
